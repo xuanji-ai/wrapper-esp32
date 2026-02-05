07bca09aa6d903ce4de75fe7576919912ab1f5fd
Nixy4 <609610903@qq.com>
2026-02-06 00:44:01 +0800

    save

---
+++ src/app/ability.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <functional>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "wrapper/logger.hpp"
+
+class AbilityBase
+{
+
+};
